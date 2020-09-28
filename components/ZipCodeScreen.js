62a4cba99add7c3186c9e7f41bf9ebce93011a81
Octomo63 <6135512019@psu.ac.th>
2020-09-28 22:29:01 +0700

    Enter

--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, FlatList, TouchableHighlight  } from 'react-native';
+import { View, Text, FlatList, TouchableHighlight, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 const availableZipItems = [
 { place: 'Hatyai', code: '90110' },
@@ -12,10 +12,10 @@ const availableZipItems = [
 ]
 const ZipItem = ({ place, code, navigation }) => (
     <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })}>
-        <View style={styles.center} >
+            <View style={styles.center} >
             <Text>{place}</Text>
             <Text>{code}</Text>
-        </View>
+            </View>
     </TouchableHighlight>
 )
 
@@ -26,12 +26,14 @@ export default function ZipCodeScreen() {
     const navigation = useNavigation()
     return (
         <View>
+            <ImageBackground source={require('../map.gif')} style={styles.backdrop}>
             <FlatList
                 data={availableZipItems}
                 keyExtractor={_keyExtractor}
                 renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
             />
             <StatusBar style="auto" />
+            </ImageBackground>
         </View>
     );
 
@@ -41,5 +43,10 @@ const styles = StyleSheet.create({
     center: {        
         justifyContent: 'space-around',
         flexDirection: 'row'
+    },
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
     },
 })
